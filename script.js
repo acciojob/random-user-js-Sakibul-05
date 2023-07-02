1f603dffcfcff972700cdaf37450c018293912de
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-02 15:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,30 +8,31 @@ let phBtn = document.getElementById("ph");
 let fetchDataBtn = document.getElementById("getUser");
 //fetching data from API and displaying it on the webpage using async function
 
-let url = "https://randomuser.me/api/";
+let baseUrl = "https://randomuser.me/api/";
 async function getData(){
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data.results[0]);
+    let response = await fetch(baseUrl);
+    let mockResponseOne = await response.json();
+    console.log(mockResponseOne.results[0]);
     //changing userName
-    userName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`
+    const fullName = `${mockResponseOne.results[0].name.first} ${mockResponseOne.results[0].name.last}`;
+	userName.innerText = fullName;
 
     // console.log(data.results[0].picture.large);
     //changing userImage
-    userImage.src = `${data.results[0].picture.large}`
+    userImage.src = `${mockResponseOne.results[0].picture.large}`
 
     aditionalInfo.innerText = "";
     ageBtn.addEventListener("click", (e)=>{
         // console.log("Age: ",data.results[0].dob.age);
-        aditionalInfo.innerText = `${data.results[0].dob.age}`
+        aditionalInfo.innerText = `${mockResponseOne.results[0].dob.age}`
     })
     emailBtn.addEventListener("click", ()=>{
         // console.log(data.results[0].email);
-        aditionalInfo.innerText = `${data.results[0].email}`
+        aditionalInfo.innerText = `${mockResponseOne.results[0].email}`
     })
     phBtn.addEventListener("click",()=>{
         // console.log(data.results[0].phone);
-        aditionalInfo.innerText = `${data.results[0].phone}`;
+        aditionalInfo.innerText = `${mockResponseOne.results[0].phone}`;
     })
     
 }
